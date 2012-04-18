86be5883e5b49e25163f7cea96842d6763f52812
Taro Minowa <higepon@twitter.com>
2012-04-18 11:26:58 +0900

    Check the checkbox. Yay!

--- api/scraper.rb
+++ api/scraper.rb
@@ -6,13 +6,17 @@ require 'mechanize'
 config = YAML.load_file("#{Dir::pwd}/config.yml")
 agent = Mechanize.new
 
-target = 'VAV17E-01'
+target = 'tcboxVAV017EI001'
 
 agent.get(config["url"]) do |page|
   menu_form = page.form_with(:name => 'Form1')
   pp menu_form
   temperature_button = menu_form.button_with(:value => '室温調節')
-  pp menu_form.submit(temperature_button)
+  kucho_page = menu_form.submit(temperature_button)
+  form = kucho_page.form_with(:name => 'Form1')
+  pp form
+  target_checkbox = form.checkbox_with(:name => target)
+  pp target_checkbox.check
 end
 
 
