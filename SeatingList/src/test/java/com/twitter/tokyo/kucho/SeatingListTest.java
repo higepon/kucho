638203738d5be12e335e64ed68de5e135a279a38
Ken Kawamoto <kentaro@twitter.com>
2012-04-18 17:18:44 +0900

    added Apache 2.0 license statement.

--- SeatingList/src/test/java/com/twitter/tokyo/kucho/SeatingListTest.java
+++ SeatingList/src/test/java/com/twitter/tokyo/kucho/SeatingListTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.twitter.tokyo.kucho;
 
 import java.util.List;
@@ -11,9 +26,6 @@ import com.twitter.tokyo.kucho.VentilationModule;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
-/**
- * @author Ken Kawamoto
- */
 public class SeatingListTest {
   private static final String JSON_URL = "https://spreadsheets.google.com/feeds/cells/0ApJM4Av2i1wSdDlWNjM0YWlJdUI4SktZdUtFOFZscnc/od6/public/basic?alt=json";
   private final String userName = "kentaro";
