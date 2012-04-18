6b48778fc9281762d9f01c94743ff905d3cb5ad3
Taro Minowa <higepon@twitter.com>
2012-04-18 13:37:26 +0900

    Fetch current temperature.

--- api/scraper.rb
+++ api/scraper.rb
@@ -3,7 +3,6 @@ require 'pp'
 require 'yaml'
 require 'mechanize'
 
-config = YAML.load_file("#{Dir::pwd}/config.yml")
 
 def change_tempr(conditioners, tempr)
   begin
@@ -49,4 +48,41 @@ def change_tempr(conditioners, tempr)
   end
 end
 
-change_tempr([], 3)
+#change_tempr([], 3)
+def current_value()
+  config = YAML.load_file("#{Dir::pwd}/config.yml")
+  dev_id = 'tcboxABU017EP015'
+  agent = Mechanize.new
+
+  target = 'tcboxVAV017EI001'
+
+  agent.get(config["url"]) do |page|
+
+    # Top menu
+    menu_form = page.form_with(:name => 'Form1')
+    pp menu_form
+
+    # Click the enter button
+    temperature_button = menu_form.button_with(:name => 'btnMainMenu1')
+    kucho_page = menu_form.submit(temperature_button)
+
+    # Select air conditioners
+    form = kucho_page.form_with(:name => 'Form1')
+    pp form
+    target_checkbox = form.checkbox_with(:name => dev_id)
+    pp target_checkbox.check
+
+    # Submit then the air conditioners are shown
+    kiki_button = form.button_with(:name => 'btnEquipment')
+    pp kiki_button
+    reloaded_page = form.submit(kiki_button)
+    confirm_form = reloaded_page.form_with(:name => 'Form1')
+
+    pp reloaded_page.search('#tblTemperatureList tr:nth-child(1) td:nth-child(4)').text.to_i
+    # id="tblTemperatureList"
+  end
+end
+
+
+
+current_value
