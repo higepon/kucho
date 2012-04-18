de3d0a3feb2ec15efa1e1c632a223bffc1e3e6b1
Taro Minowa <higepon@twitter.com>
2012-04-18 15:19:46 +0900

    Added name converter

--- api/call_api.rb
+++ api/call_api.rb
@@ -2,6 +2,12 @@ require 'rubygems'
 require 'yaml'
 require 'api/air_remote'
 
+def name2dev_id(name)
+  return "tcbox#{name[0..2]}0#{name[3..5]}I0#{name[7..8]}"
+end
+
 config = YAML.load_file("#{Dir::pwd}/config.yml")
+dev_ids = ARGV.map {|arg| name2dev_id(arg) }
+
 remote = AirRemote.new(config["url"])
-remote.cooler!(['tcboxVAV017EI001', 'tcboxVAV017EI002'])
+remote.cooler!(dev_ids)
