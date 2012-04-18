4ce925f90809d1794110edc6d68e8d2a62097c08
Taro Minowa <higepon@twitter.com>
2012-04-18 11:53:54 +0900

    Added comment.

--- api/scraper.rb
+++ api/scraper.rb
@@ -9,23 +9,33 @@ agent = Mechanize.new
 target = 'tcboxVAV017EI001'
 
 agent.get(config["url"]) do |page|
+
+  # Top menu
   menu_form = page.form_with(:name => 'Form1')
   pp menu_form
+
+  # Click the enter button
   temperature_button = menu_form.button_with(:name => 'btnMainMenu1')
   kucho_page = menu_form.submit(temperature_button)
+
+  # Select air conditioners
   form = kucho_page.form_with(:name => 'Form1')
   pp form
   target_checkbox = form.checkbox_with(:name => target)
   pp target_checkbox.check
+
+  # Submit then the air conditioners are shown
   kiki_button = form.button_with(:name => 'btnEquipment')
   pp kiki_button
   reloaded_page = form.submit(kiki_button)
   confirm_form = reloaded_page.form_with(:name => 'Form1')
 
+  # Select temperature
   temp_button =  confirm_form.button_with(:name => 'btnTemperatureSetValueButtons2')
   last_page = confirm_form.submit(temp_button)
   pp last_page
   last_form = last_page.form_with(:name => 'Form1')
+  # Go!
   pp last_form.submit(last_form.button_with(:name => 'btnAdjust'))
 end
 
