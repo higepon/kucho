dadb1cb9176d25de72b5e8e51b5d798916bc45e8
Taro Minowa <higepon@twitter.com>
2012-04-18 11:09:04 +0900

    Click the menu button. Yay.

--- api/scraper.rb
+++ api/scraper.rb
@@ -5,6 +5,15 @@ require 'mechanize'
 
 config = YAML.load_file("#{Dir::pwd}/config.yml")
 agent = Mechanize.new
-page = agent.get(config["url"])
-pp page
+
+target = 'VAV17E-01'
+
+agent.get(config["url"]) do |page|
+  menu_form = page.form_with(:name => 'Form1')
+  pp menu_form
+  temperature_button = menu_form.button_with(:value => '室温調節')
+  pp menu_form.submit(temperature_button)
+end
+
+
 
